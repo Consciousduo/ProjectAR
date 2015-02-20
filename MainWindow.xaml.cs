22db2d0ace7b0be8371edde69e4bc59eb4b47223
Consciousduo <duozhao@usc.edu>
2015-02-19 19:21:23 -0800

    final

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,362 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace Project_AR
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+    using Microsoft.Kinect;
+    using Microsoft.Kinect.Toolkit;
+    using Microsoft.Kinect.Toolkit.Controls;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+
+    /// <summary>
+    /// Interaction logic for MainWindow
+    /// </summary>
+    public partial class MainWindow
+    {
+        public static readonly DependencyProperty PageLeftEnabledProperty = DependencyProperty.Register(
+            "PageLeftEnabled", typeof(bool), typeof(MainWindow), new PropertyMetadata(false));
+
+        public static readonly DependencyProperty PageRightEnabledProperty = DependencyProperty.Register(
+            "PageRightEnabled", typeof(bool), typeof(MainWindow), new PropertyMetadata(false));
+
+        private const double ScrollErrorMargin = 0.001;
+
+        private const int PixelScrollByAmount = 20;
+
+        //private readonly KinectSensorChooser sensorChooser;
+
+
+        private static readonly int Bgr32BytesPerPixel = (PixelFormats.Bgr32.BitsPerPixel + 7) / 8;
+        private WriteableBitmap colorImageWritableBitmap;
+        private byte[] colorImageData;
+        private ColorImageFormat currentColorImageFormat = ColorImageFormat.Undefined;
+        private readonly KinectSensorChooser sensorChooser = new KinectSensorChooser();
+        private int whichStream;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class. 
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            // initialize the sensor chooser and UI
+            //this.sensorChooser = new KinectSensorChooser();
+
+            var faceTrackingViewerBinding = new Binding("Kinect") { Source = sensorChooser };
+            faceTrackingViewer.SetBinding(FaceTrackingViewer.KinectProperty, faceTrackingViewerBinding);
+            
+            sensorChooser.KinectChanged += SensorChooserOnKinectChanged;
+           // this.sensorChooserUi.KinectSensorChooser = this.sensorChooser;
+            sensorChooser.Start();
+
+            // Bind the sensor chooser's current sensor to the KinectRegion
+            var regionSensorBinding = new Binding("Kinect") { Source = sensorChooser };
+            BindingOperations.SetBinding(this.kinectRegion, KinectRegion.KinectSensorProperty, regionSensorBinding);
+
+            // Clear out placeholder content
+            /*this.wrapPanel.Children.Clear();
+
+            // Add in display content
+            for (var index = 0; index < 10; ++index)
+            {
+                var button = new KinectTileButton { Label = (index + 1).ToString(CultureInfo.CurrentCulture) };
+                this.wrapPanel.Children.Add(button);
+            }
+             */
+
+            // Bind listner to scrollviwer scroll position change, and check scroll viewer position
+            this.UpdatePagingButtonState();
+            scrollViewer.ScrollChanged += (o, e) => this.UpdatePagingButtonState();
+        }
+
+        /// <summary>
+        /// CLR Property Wrappers for PageLeftEnabledProperty
+        /// </summary>
+        public bool PageLeftEnabled
+        {
+            get
+            {
+                return (bool)GetValue(PageLeftEnabledProperty);
+            }
+
+            set
+            {
+                this.SetValue(PageLeftEnabledProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// CLR Property Wrappers for PageRightEnabledProperty
+        /// </summary>
+        public bool PageRightEnabled
+        {
+            get
+            {
+                return (bool)GetValue(PageRightEnabledProperty);
+            }
+
+            set
+            {
+                this.SetValue(PageRightEnabledProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// Called when the KinectSensorChooser gets a new sensor
+        /// </summary>
+        /// <param name="sender">sender of the event</param>
+        /// <param name="args">event arguments</param>
+        /// 
+        /*
+        private void SensorChooserOnKinectChanged(object sender, KinectChangedEventArgs kinectChangedEventArgs)
+        {
+            KinectSensor oldSensor = kinectChangedEventArgs.OldSensor;
+            KinectSensor newSensor = kinectChangedEventArgs.NewSensor;
+
+            if (oldSensor != null)
+            {
+                oldSensor.AllFramesReady -= KinectSensorOnAllFramesReady;
+                oldSensor.ColorStream.Disable();
+                oldSensor.DepthStream.Disable();
+                oldSensor.DepthStream.Range = DepthRange.Default;
+                oldSensor.SkeletonStream.Disable();
+                oldSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                oldSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Default;
+            }
+
+            if (newSensor != null)
+            {
+                try
+                {
+                    newSensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                    newSensor.DepthStream.Enable(DepthImageFormat.Resolution320x240Fps30);
+                    try
+                    {
+                        // This will throw on non Kinect For Windows devices.
+                        newSensor.DepthStream.Range = DepthRange.Near;
+                        newSensor.SkeletonStream.EnableTrackingInNearRange = true;
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        newSensor.DepthStream.Range = DepthRange.Default;
+                        newSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                    }
+
+                    newSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+                    newSensor.SkeletonStream.Enable();
+                    newSensor.AllFramesReady += KinectSensorOnAllFramesReady;
+                }
+                catch (InvalidOperationException)
+                {
+                    // This exception can be thrown when we are trying to
+                    // enable streams on a device that has gone away.  This
+                    // can occur, say, in app shutdown scenarios when the sensor
+                    // goes away between the time it changed status and the
+                    // time we get the sensor changed notification.
+                    //
+                    // Behavior here is to just eat the exception and assume
+                    // another notification will come along if a sensor
+                    // comes back.
+                }
+            }
+        }
+         */
+       
+        
+       
+        private void SensorChooserOnKinectChanged(object sender, KinectChangedEventArgs args)
+        {
+           
+            if (args.OldSensor != null)
+            {
+                try
+                {
+                    
+                   args.OldSensor.AllFramesReady -= KinectSensorOnAllFramesReady;
+                   args.OldSensor.ColorStream.Disable();
+                   args.OldSensor.DepthStream.Disable();
+                   args.OldSensor.DepthStream.Range = DepthRange.Default;
+                   args.OldSensor.SkeletonStream.Disable();
+                   args.OldSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                   args.OldSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Default;
+                  
+                }
+                catch (InvalidOperationException)
+                {
+                    // KinectSensor might enter an invalid state while enabling/disabling streams or stream features.
+                    // E.g.: sensor might be abruptly unplugged.
+                }
+            }
+
+            if (args.NewSensor != null)
+            {
+                try
+                {
+                    args.NewSensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+                    args.NewSensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+                    args.NewSensor.SkeletonStream.Enable();
+
+                    try
+                    {
+                        args.NewSensor.DepthStream.Range = DepthRange.Near;
+                        args.NewSensor.SkeletonStream.EnableTrackingInNearRange = true;
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        // Non Kinect for Windows devices do not support Near mode, so reset back to default mode.
+                        args.NewSensor.DepthStream.Range = DepthRange.Default;
+                        args.NewSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                    }
+
+                    args.NewSensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+                    args.NewSensor.SkeletonStream.Enable();
+                    args.NewSensor.AllFramesReady += KinectSensorOnAllFramesReady;
+
+                }
+                catch (InvalidOperationException)
+                {
+                    // KinectSensor might enter an invalid state while enabling/disabling streams or stream features.
+                    // E.g.: sensor might be abruptly unplugged.
+                }
+            }
+        }
+         
+        
+        /// <summary>
+        /// Execute shutdown tasks
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            this.sensorChooser.Stop();
+        }
+
+        /// <summary>
+        /// Handle a button click from the wrap panel.
+        /// </summary>
+        /// <param name="sender">Event sender</param>
+        /// <param name="e">Event arguments</param>
+        private void KinectTileButtonClick(object sender, RoutedEventArgs e)
+        {
+            var button = (KinectTileButton)e.OriginalSource;
+            var selectionDisplay = new SelectionDisplay(button.Label as string);
+            this.kinectRegionGrid.Children.Add(selectionDisplay);
+            e.Handled = true;
+        }
+
+        /// <summary>
+        /// Handle paging right (next button).
+        /// </summary>
+        /// <param name="sender">Event sender</param>
+        /// <param name="e">Event arguments</param>
+        private void PageRightButtonClick(object sender, RoutedEventArgs e)
+        {
+            scrollViewer.ScrollToHorizontalOffset(scrollViewer.HorizontalOffset + PixelScrollByAmount);
+        }
+
+        /// <summary>
+        /// Handle paging left (previous button).
+        /// </summary>
+        /// <param name="sender">Event sender</param>
+        /// <param name="e">Event arguments</param>
+        private void PageLeftButtonClick(object sender, RoutedEventArgs e)
+        {
+            scrollViewer.ScrollToHorizontalOffset(scrollViewer.HorizontalOffset - PixelScrollByAmount);
+        }
+
+        /// <summary>
+        /// Change button state depending on scroll viewer position
+        /// </summary>
+        private void UpdatePagingButtonState()
+        {
+            this.PageLeftEnabled = scrollViewer.HorizontalOffset > ScrollErrorMargin;
+            this.PageRightEnabled = scrollViewer.HorizontalOffset < scrollViewer.ScrollableWidth - ScrollErrorMargin;
+        }
+
+        private void KinectSensorOnAllFramesReady(object sender, AllFramesReadyEventArgs allFramesReadyEventArgs)
+        {
+            using (var colorImageFrame = allFramesReadyEventArgs.OpenColorImageFrame())
+            {
+                if (colorImageFrame == null)
+                {
+                    return;
+                }
+
+                // Make a copy of the color frame for displaying.
+                var haveNewFormat = this.currentColorImageFormat != colorImageFrame.Format;
+                if (haveNewFormat)
+                {
+                    this.currentColorImageFormat = colorImageFrame.Format;
+                    this.colorImageData = new byte[colorImageFrame.PixelDataLength];
+                    this.colorImageWritableBitmap = new WriteableBitmap(
+                        colorImageFrame.Width, colorImageFrame.Height, 96, 96, PixelFormats.Bgr32, null);
+                    ColorImage.Source = this.colorImageWritableBitmap;
+                }
+
+                colorImageFrame.CopyPixelDataTo(this.colorImageData);
+                this.colorImageWritableBitmap.WritePixels(
+                    new Int32Rect(0, 0, colorImageFrame.Width, colorImageFrame.Height),
+                    this.colorImageData,
+                    colorImageFrame.Width * Bgr32BytesPerPixel,
+                    0);
+            }
+
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+            //This does nothing just leave it or it messes up
+        }
+        private void ColorClick(object sender, RoutedEventArgs e)
+        {
+            //Enter code to disable other streams and enable Color Stream
+            whichStream = 1; 
+        }
+        private void SkeletonClick(object sender, RoutedEventArgs e)
+        {
+            //Enter code to disable other streams and enable Skeleton Stream
+            whichStream = 2; 
+        }
+        private void InfraredClick(object sender, RoutedEventArgs e)
+        {
+            //Enter code to disable other streams and enable Skeleton Stream
+            whichStream = 3; 
+        }
+
+        private void ExitClick(object sender, RoutedEventArgs e)
+        {
+            this.sensorChooser.Stop();
+            Environment.Exit(0);
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            AboutBox1 box = new AboutBox1();
+            box.Show();
+        }
+
+        private void facetrackdisable(object sender, RoutedEventArgs e)
+        {
+            bool b = Properties.Settings.Default.Facetracking;
+            if (b)
+            {
+                Properties.Settings.Default.Facetracking = false;
+            }
+            else
+            {
+                Properties.Settings.Default.Facetracking = true;
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Kinect for Windows Developer Toolkit Controls Basics - WPF")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+    //(used if a resource is not found in the page, 
+    // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+    //(used if a resource is not found in the page, 
+    // app, or any theme specific resource dictionaries)
+)]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+[assembly: CLSCompliant(true)]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
